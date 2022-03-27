9fe3ceb32049ae6056e8d6c1afad54cc6d5a7ee2
tapia1013 <manbearpig1013@gmail.com>
2022-03-26 21:08:45 -0700

    homepage and styling done anc creating createuser

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,8 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet" />
+          <script src="https://kit.fontawesome.com/d42159e0f7.js" crossorigin="anonymous"></script>
         </Head>
         <body>
           <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 
 
@@ -8,7 +7,28 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      test
+      <div className='login-user'>
+        <div className='login-user__top'>
+          <div className='login-user__logo' />
+          <span className='login-user__title'>
+            Who Is Watching?
+          </span>
+        </div>
+
+        <div className='login-user__form'>
+          <div className='login-user__user-box'>
+            <img className='login-user__user-img' src='https://www.dogbreedslist.info/uploads/dog-pictures/maltese-1.jpg' />
+            <div className='login-user__user-name'>
+              Cookie
+            </div>
+          </div>
+        </div>
+
+        <div className='login-user__buttons'>
+          <button className='login-user__adult'>Add Adult</button>
+          <button className='login-user__kid'>Add Kid</button>
+        </div>
+      </div>
     </div>
   )
 }

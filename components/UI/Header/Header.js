6dc60c28ac000da767b582023229e24d1e6285fe
tapia1013 <manbearpig1013@gmail.com>
2022-03-28 14:49:38 -0700

    moved header and sidenav to own folder with their correspoding style sheets and finished sidenav

---
+++ components/UI/Header/Header.js
@@ -0,0 +1,24 @@
+const Header = (props) => {
+  return (
+    <header className="top-header">
+      <div className="top-header__left-side">
+        <div className="top-header__menu-btn">
+          <i className="fas fa-bars" />
+        </div>
+
+        <div className="top-header__search-btn">
+          <i className="fas fa-search" />
+        </div>
+      </div>
+
+      <div className="top-header__logo"></div>
+
+      <div className="top-header__account">
+        <img src="https://www.dogbreedslist.info/uploads/dog-pictures/maltese-1.jpg" className="top-header__user-img" />
+        <div className="top-header__user-name">Cookie</div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ pages/home.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import MainLayout from '../components/layouts/MainLayout'
+
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <div>
+        home
+      </div>
+    </MainLayout>
+  )
+}

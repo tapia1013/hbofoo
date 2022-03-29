6fd6866f0e26cb742cac38cb740e7e8b5b9668c0
tapia1013 <manbearpig1013@gmail.com>
2022-03-29 16:24:58 -0700

    lazyload

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ import MainLayout from '../components/Layouts/MainLayout'
 import FeaturedMedia from '../components/UI/FeaturedMedia/FeaturedMedia';
 import MediaRow from '../components/UI/MediaRow/MediaRow'
 import AuthCheck from '../components/AuthCheck';
+import LazyLoad from 'react-lazyload';
+
 
 
 export default function Home() {
@@ -25,36 +27,48 @@ export default function Home() {
   return AuthCheck(
     <MainLayout>
       <FeaturedMedia />
-      <MediaRow
-        title="Movies"
-        type='large-v'
-        endpoint='discover/movie?sort_by=popularity.desc&primary_release_year=2022'
-      />
-      <MediaRow
-        title="Series"
-        type='small-h'
-        endpoint='discover/tv?primary_release_year=2022'
-      />
-      <MediaRow
-        title="Action"
-        type='small-v'
-        endpoint='discover/movie?with_genres=28&primary_release_year=2022'
-      />
-      <MediaRow
-        title="Horror"
-        type='large-h'
-        endpoint='discover/movie?with_genres=27&primary_release_year=2022'
-      />
-      <MediaRow
-        title="Animations"
-        type='large-h'
-        endpoint='discover/movie?with_genres=16&primary_release_year=2022'
-      />
-      <MediaRow
-        title="Sci-fi"
-        type='small-v'
-        endpoint='discover/movie?with_genres=878&primary_release_year=2022'
-      />
+      <LazyLoad height={680} offset={-200} placeholder={<h1>Loading</h1>}>
+        <MediaRow
+          title="Movies"
+          type='large-v'
+          endpoint='discover/movie?sort_by=popularity.desc&primary_release_year=2022'
+        />
+      </LazyLoad>
+      <LazyLoad height={333} offset={-200}>
+        <MediaRow
+          title="Series"
+          type='small-h'
+          endpoint='discover/tv?primary_release_year=2022'
+        />
+      </LazyLoad>
+      <LazyLoad height={440} offset={-200}>
+        <MediaRow
+          title="Action"
+          type='small-v'
+          endpoint='discover/movie?with_genres=28&primary_release_year=2022'
+        />
+      </LazyLoad>
+      <LazyLoad height={200}>
+        <MediaRow
+          title="Horror"
+          type='large-h'
+          endpoint='discover/movie?with_genres=27&primary_release_year=2022'
+        />
+      </LazyLoad>
+      <LazyLoad height={200}>
+        <MediaRow
+          title="Animations"
+          type='large-h'
+          endpoint='discover/movie?with_genres=16&primary_release_year=2022'
+        />
+      </LazyLoad>
+      <LazyLoad height={200}>
+        <MediaRow
+          title="Sci-fi"
+          type='small-v'
+          endpoint='discover/movie?with_genres=878&primary_release_year=2022'
+        />
+      </LazyLoad>
     </MainLayout>
   )
 }
